b9c1b293df65a4cb956d5854880f421518aeb5f0
yinm5 <yinm5@ziroom.com>
2017-03-19 23:54:49 +0800

    tt

--- src/main/java/testng/Comments.java
+++ src/main/java/testng/Comments.java
@@ -5,8 +5,9 @@ import org.testng.annotations.Test;
 /**
  * Created by admin on 2017/3/13.
  */
+
 public class Comments extends Annotations {
-    @Test
+    @Test(successPercentage = 100)
     public void methodChild(){
         System.out.println("Commnents Method Running");
         //methodBase();
